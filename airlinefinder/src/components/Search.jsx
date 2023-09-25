9c4ff590048980183ff6f0a678c8b21d9b367d5e
Sakib Islam <islamsakib.official@gmail.com>
2023-09-25 21:16:03 +0600

    Update 25 sep 2023

--- airlinefinder/src/components/Search.jsx
+++ airlinefinder/src/components/Search.jsx
@@ -1,26 +1,19 @@
 import React, { useState } from "react";
 
 export default function Search() {
-  let [entry, NewENtry] = useState("");
-
-  const onInputChange = (event) => {
-    console.log(event.target.value)
+  const [input, setInput] = useState("");
+  const onFormSubmit = (event) => {
+    event.target.value = setInput;
+    event.preventDefault();
+    console.log(input);
   };
-
-  const onFormSubmit = (event) => { 
-    event.preventDefault()
-   }
 
   return (
     <div className="ui segment">
       <form onSubmit={onFormSubmit} className="ui form" action="">
         <div className="field">
           <div className="ui massive icon input">
-            <input
-              onChange={onInputChange}
-              type="text"
-              placeholder="search..."
-            />
+            <input type="text" placeholder="search..." />
             <i className="search icon"></i>
           </div>
         </div>
