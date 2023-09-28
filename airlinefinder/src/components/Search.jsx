6adb594ce44d2461dcafff754640b130a0babc26
Sakib Islam <islamsakib.official@gmail.com>
2023-09-28 12:39:40 +0600

    Date 28-sep 2023 12:39Pm

--- airlinefinder/src/components/Search.jsx
+++ airlinefinder/src/components/Search.jsx
@@ -5,18 +5,25 @@ export default class Search extends Component {
     super(props);
     this.state = { entry: "" }; //Setting an empty string state to store user input
   }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.entry)
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" action="">
+        <form onSubmit={this.onFormSubmit} className="ui form" action="">
           <div className="field">
             <div className="ui massive icon input">
               <input
+                type="text"
+                placeholder="search..."
                 onChange={(event) => {
                   this.setState({ entry: event.target.value }); // Setting an state to update user input to the empty string stste
                 }}
-                type="text"
-                placeholder="search..."
+                value={this.state.entry}
               />
               <i className="search icon"></i>
             </div>
