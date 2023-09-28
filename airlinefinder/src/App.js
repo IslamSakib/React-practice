ffe0f66318ff294105eea3316513a9e8ef8b0c4d
Sakib Islam <islamsakib.official@gmail.com>
2023-09-28 11:27:14 +0600

    Date 28-sep-2023

--- airlinefinder/src/App.js
+++ airlinefinder/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="ui container">
-        <Search />
+        <Search/>
       </div>
     </div>
   );
--- airlinefinder/src/components/Search.jsx
+++ airlinefinder/src/components/Search.jsx
@@ -1,23 +1,18 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-export default function Search() {
-  const [input, setInput] = useState("");
-  const onFormSubmit = (event) => {
-    event.target.value = setInput;
-    event.preventDefault();
-    console.log(input);
-  };
-
-  return (
-    <div className="ui segment">
-      <form onSubmit={onFormSubmit} className="ui form" action="">
-        <div className="field">
-          <div className="ui massive icon input">
-            <input type="text" placeholder="search..." />
-            <i className="search icon"></i>
+export default class Search extends Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" action="">
+          <div className="field">
+            <div className="ui massive icon input">
+              <input type="text" placeholder="search..." />
+              <i className="search icon"></i>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
+  }
 }
